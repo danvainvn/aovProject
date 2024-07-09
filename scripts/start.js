8efc8a49ea595deafbf2e808f589812038da7221
Vinh <159706493+danvainvn@users.noreply.github.com>
2024-07-09 14:51:33 +0700

    Add files via upload

--- scripts/start.js
+++ scripts/start.js
@@ -1,3 +1,9 @@
 $('.startPluginbtn').click(function(){
     $("#preloader-start").delay(100).fadeOut("10000");
 });
+
+$("#registered").fadeOut("0");
+$('.submit').click(function(){
+    $("#registered").fadeIn("0");
+    $("#registered").delay(3000).fadeOut("0");
+});
--- scripts/swiper.js
+++ scripts/swiper.js
@@ -1,7 +1,7 @@
 let swiperCards = new Swiper('.cardContent', {
     loop: true,
     spaceBetween: 32,
-    grabCursor: true,
+    grabCursor: false,
   
     pagination: {
       el: '.swiper-pagination',
@@ -20,7 +20,7 @@ let swiperCards = new Swiper('.cardContent', {
 let swiperCards2 = new Swiper('.cardContent2', {
   loop: true,
   spaceBetween: 32,
-  grabCursor: true,
+  grabCursor: false,
 
   pagination: {
     el: '.swiper-pagination',
